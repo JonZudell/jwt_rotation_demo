0ec61ce05d9d565ff98d52ad27f3a1116ce3c0e9
Jon Zudell <jonathon.zudell@gmail.com>
2025-03-28 08:11:22 -0500

    examples

--- gen_rsa_keypairs.py
+++ gen_rsa_keypairs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
--- jwt_from_payload.py
+++ jwt_from_payload.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import json
 import base64
 import sys
@@ -5,19 +6,14 @@ from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
 
 
-# Function to Base64URL encode
 def b64url_encode(data: bytes) -> str:
     return base64.urlsafe_b64encode(data).rstrip(b"=").decode("utf-8")
 
 
 # Create JWT header and payload
 header = {"alg": "RS256", "typ": "JWT"}
-
-# Encode header and payload
 header_b64 = b64url_encode(json.dumps(header, separators=(",", ":")).encode("utf-8"))
 payload_b64 = b64url_encode(sys.argv[1].encode("utf-8"))
-
-# Create the signing input
 signing_input = f"{header_b64}.{payload_b64}".encode("utf-8")
 
 # Load the private key
@@ -25,16 +21,13 @@ private_key_pem = None
 with open("keypair.pem", "rb") as key_file:
     private_key_pem = key_file.read()
 
-
 private_key = serialization.load_pem_private_key(
     private_key_pem,
     password=None,
 )
 
-# Sign the input
+# Sign the input and encode the input
 signature = private_key.sign(signing_input, padding.PKCS1v15(), hashes.SHA256())
-
-# Base64URL encode the signature
 signature_b64 = b64url_encode(signature)
 
 # Construct the JWT
